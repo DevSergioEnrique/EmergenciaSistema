b50192a1a7050d737ca9e126d155f0a4f94dc482
FernandoYZ <fernandozavala266@gmail.com>
2025-03-06 18:24:26 -0500

    feat(registro): mejorar consulta de registros de emergencia

--- app/models/RegistroModel.php
+++ app/models/RegistroModel.php
@@ -7,30 +7,41 @@ class RegistroModel extends Model {
 
     public function getRegistroEmergencia() {
         $sql = "SELECT 
-            RE.IdTriajeEmergencia,
-            TDI.Descripcion AS TipoDocumento,
-            ISNULL(RE.NroDocPaciente, 'Sin Documento') AS NroDocumento, 
-            RE.Paciente, 
-            E.Nombres + ' ' + E.ApellidoPaterno AS Medico, 
-            D.Descripcion AS Diagnostico,
-            E2.Nombres + ' ' + E2.ApellidoPaterno AS Usuario,
-            RE.Fecha_Hora
-        FROM 
-            RegistroEmergencia RE
-        INNER JOIN 
-            TiposDocIdentidad TDI ON RE.IdTipoDocumento = TDI.IdDocIdentidad
-        INNER JOIN 
-            Medicos M ON RE.IdMedico = M.IdMedico
-        INNER JOIN 
-            Empleados E ON M.IdEmpleado = E.IdEmpleado 
-        INNER JOIN 
-            Diagnosticos D ON RE.IdDiagnostico = D.IdDiagnostico
-        INNER JOIN 
-            Empleados E2 ON RE.IdUsuario = E2.IdEmpleado
-        WHERE 
-            YEAR(RE.Fecha_Hora) = YEAR(GETDATE()) 
-        ORDER BY 
-            RE.Fecha_Hora DESC;";
+	re.IdTriajeEmergencia AS IdTriajeEmergencia,
+	tdi.Descripcion AS Documento,
+	COALESCE(NULLIF(re.NroDocPaciente, ''), 'Sin Documento') AS Nro,
+	re.Paciente AS Paciente,
+	fi.Descripcion AS FormaIngreso,
+	mi.Descripcion AS MotivoIngreso,
+	s.Nombre AS Servicio,
+	re.Medico AS Medico,
+	tga.Descripcion AS Gravedad,
+	re.CodigoDiagnostico AS CodigoDiagnostico,
+	d.Descripcion AS DescripcionDiagnostico,
+	sb.Descripcion AS TipoDiagnostico,
+	tf.Descripcion AS Financiamiento,
+	e.Usuario AS Usuario,
+	re.Fecha_Hora AS FechaHora
+FROM 
+	dbo.RegistroEmergencia AS re
+		INNER JOIN 
+	dbo.TiposDocIdentidad AS tdi ON re.IdTipoDocumento = tdi.IdDocIdentidad
+		INNER JOIN
+	dbo.FormaIngresoEmer AS fi ON re.IdFormaIngreso = fi.IdFormaIngreso
+		INNER JOIN 
+	dbo.MotivoIngresoEmer AS mi ON re.IdMotivoIngreso = mi.IdMotivoIngreso
+		INNER JOIN
+	dbo.Servicios AS s ON re.IdServicio = s.IdServicio
+		INNER JOIN
+	dbo.TiposGravedadAtencion AS tga ON re.IdGravedad = tga.IdTipoGravedad
+		INNER JOIN
+	(SELECT IdDiagnostico, CodigoCIE10, Descripcion FROM dbo.Diagnosticos) AS d ON re.CodigoDiagnostico + '     ' = d.CodigoCIE10
+		INNER JOIN
+	dbo.SubclasificacionDiagnosticos AS sb ON re.IdTipoDiagnostico = sb.IdSubclasificacionDx
+		INNER JOIN
+	dbo.TiposFinanciamiento AS tf ON re.IdFinanciamiento = tf.IdTipoFinanciamiento
+		INNER JOIN
+	dbo.Empleados AS e ON re.IdUsuario = e.IdEmpleado";
 
                 $prueba = $this->selectAll($sql);
                 return $prueba ?: ["error" => "No se encontraron registros de emergencia"];
